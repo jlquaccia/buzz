9ecb44f73dc8262c7d5543dbb072ee7bd28b7197
Jason Quaccia <jlquaccia@gmail.com>
2016-09-22 12:33:12 -0700

    able to find the current location on map of user

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,27 @@
 (function() {
-  function HomeCtrl($scope, $stateParams, $rootScope, uiGmapGoogleMapApi, LocalStorage) {
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-    
-    uiGmapGoogleMapApi.then(function(maps) {
+  function HomeCtrl($scope, $stateParams, $rootScope, $timeout, uiGmapGoogleMapApi, LocalStorage) {
+    if (localStorage.currentLocation) {
+      $rootScope.map = LocalStorage.get('currentLocation');
+    } else {
+      $rootScope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+    }
 
+    uiGmapGoogleMapApi.then(function(maps) {
+      var options = {
+        enableHighAccuracy: true
+      };
+
+      navigator.geolocation.getCurrentPosition(function(pos) {
+        $scope.position = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+        console.log(JSON.stringify($scope.position));
+        $timeout(function() {
+          var currentLocation = { center: {latitude: pos.coords.latitude, longitude: pos.coords.longitude}, zoom: 10 };
+          LocalStorage.set('currentLocation', currentLocation);
+          $rootScope.map = currentLocation;
+        }, 0);
+      }, function(error) {
+        alert('Unable to get location: ' + error.message);
+      }, options);
     });
 
     if (!$rootScope.loggedIn) {
@@ -15,5 +33,5 @@
 
   angular
     .module('buzz')
-    .controller('HomeCtrl', ['$scope', '$stateParams', '$rootScope', 'uiGmapGoogleMapApi', 'LocalStorage', HomeCtrl]);
+    .controller('HomeCtrl', ['$scope', '$stateParams', '$rootScope', '$timeout', 'uiGmapGoogleMapApi', 'LocalStorage', HomeCtrl]);
 })();
