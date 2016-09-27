45064d1f7b5a2b748b17c39a16a69ccd475ab2a8
Jason Quaccia <jlquaccia@gmail.com>
2016-09-26 22:43:11 -0700

    added all ig user media tagged with location as pins onto a google map

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -12,17 +12,38 @@
 
     if (localStorage.currentLocationMapOptions) {
       var map = new google.maps.Map(document.getElementById('map-canvas'), LocalStorage.get('currentLocationMapOptions')),
-          marker = new google.maps.Marker({
+          currentLocationMarker = new google.maps.Marker({
             position: LocalStorage.get('currentLocation'),
             map: map,
             title: 'You are here'
           });
-
-      console.log('initMap was not invoked');
     } else {
-      console.log('invoked initMap');
       initMap();
     }
+
+    $rootScope.currentUserRecentMedia.forEach(function(media) {
+      if (media.location) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(media.location.latitude, media.location.longitude),
+          map: map,
+          animation: google.maps.Animation.DROP,
+          icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+        });
+      }
+    });
+
+    $rootScope.currentUserFollowsRecentMedia.forEach(function(user) {
+      user.forEach(function(item) {
+        if (item.location) {
+          marker = new google.maps.Marker({
+            position: new google.maps.LatLng(item.location.latitude, item.location.longitude),
+            map: map,
+            animation: google.maps.Animation.DROP,
+            icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+          });
+        }
+      });
+    });
   }
 
   angular
